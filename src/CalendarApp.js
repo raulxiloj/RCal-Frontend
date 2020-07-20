682f8b6e567ca102a3427a2f4d56924b6251de8f
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-07-20 11:23:51 -0600

    Login and register desing created. Also create a simple navbar for the calendar screen

--- src/CalendarApp.js
+++ src/CalendarApp.js
@@ -3,8 +3,6 @@ import { AppRouter } from './router/AppRouter'
 
 export const CalendarApp = () => {
     return (
-        <div>
-            <AppRouter />
-        </div>
+        <AppRouter />
     )
 }
--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './login.css'
 
 export const LoginScreen = () => {
+
+    const [formSign, setForm] = useState(true); //If true = signin || false = signup
+
     return (
-        <div>
-            <h1>LogginScreen</h1>
+        <div className="container-2">
+            <div className={formSign ? 'container' : 'container right-panel-active'} id="container">
+
+                <div className="form-container sign-up-container">
+                    <form >
+                        <h1>Create Account</h1>
+                        <input type="text" placeholder="Name" />
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <button >Sign Up</button>
+                    </form>
+                </div>
+
+                <div className="form-container sign-in-container">
+                    <form >
+                        <h1 className="mb-4">Sign in</h1>
+                        <input type="email" placeholder="Email" />
+                        <input type="password" placeholder="Password" />
+                        <a href="/">Forgot your password?</a>
+                        <button>Sign In</button>
+                    </form>
+                </div>
+
+                <div className="overlay-container">
+                    <div className="overlay">
+                        <div className="overlay-panel overlay-left">
+                            <h1>Welcome Back!</h1>
+                            <p>To keep connected with us please login with your personal info</p>
+                            <button className="ghost" id="signIn" onClick={() => { setForm(true) }}>Sign In</button>
+                        </div>
+                        <div className="overlay-panel overlay-right">
+                            <h1>Hello, Friend!</h1>
+                            <p>Enter your personal details and start journey with us</p>
+                            <button className="ghost" id="signUp" onClick={() => { setForm(false) }}>Sign Up</button>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
         </div>
+
     )
 }
