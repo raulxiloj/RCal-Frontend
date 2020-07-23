2d8e91a4cee9eb5a7d7d1696cc08ab112078d3c0
Raul Xiloj <2990544030101@ingenieria.usac.edu.gt>
2020-07-23 16:28:51 -0600

    [add] modal and some events added to the calendar screen

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Navbar = () => {
     return (
-        <div className="navbar navbar-dark bg-dark mb-4">
+        <div className="navbar navbar-dark mb-4" style={{ backgroundColor: 'black' }}>
             <span className="navbar-brand"> Raul </span>
             <button className="btn btn-outline-danger">
                 <i className="fas fa-sign-out-alt"></i>
